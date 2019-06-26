d35b8305298a2e10b9acb44350bd89841b39476e
George Shafik <george@gsss.net>
2019-06-26 10:02:38 +1000

    Refactored to common crud interface. Closes #11

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.services;
+
+import java.util.Set;
+
+/**
+ * Created by gs on 2019-06-26
+ *
+ * We are refactoring out the common services into a common interface
+ * similar to the way Spring Crud Repository through generics
+ *
+ */
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+}

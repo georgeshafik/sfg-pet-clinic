95e8fce0323a89405470d3edc75f688da4168d29
George Shafik <george@gsss.net>
2019-06-26 17:46:31 +1000

    Added Index Page and Controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by gs on 2019-06-26
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        // thymeleaf will look for a template called index.html
+        return "index";
+    }
+}

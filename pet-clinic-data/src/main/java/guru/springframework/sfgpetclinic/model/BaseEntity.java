c0c41df38a649d0cbb9ad14571cc435690487fc4
George Shafik <george@gsss.net>
2019-07-03 10:54:46 +1000

    Implement Base Entity (JPA). Closed #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by gs on 2019-06-25
  */
+// This establishes as a base class for JPA.
+// JPA will know to inherit from this class.
+// JPA will not map this specific class to the database.
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // remember we are targeting MySQL and H2 - so we want the databased to manager strategy
     private Long id;
 
     public Long getId() {

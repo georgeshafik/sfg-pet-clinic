dea4ed445b864ecf0dc28dcfaf4c62fc282a5b1c
George Shafik <george@gsss.net>
2019-07-04 14:27:06 +1000

    Create Spring Data JPA service for Visit. Closed #49.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,58 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repository.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by gs on 2019-07-04
+ */
+// Remember we need to set this as an active profile for it to be used. Currently it will use
+// VisitServiceMap by default if we deactivate VisitSDJpaService by introducing the @Profile
+// Recap: we have two services defined for VisitService
+// 1. VisitServiceMap
+// 2. VisitSDJpaService
+// Only one vet service can be used at runtime.
+// Using a profile ensures we activate or de-active a given visit service ensuring only one is used.
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
